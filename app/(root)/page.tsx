4ae5656dd853522af0774c7e4a895d9484852bf5
AazimAnish <aazimanish1@gmail.com>
2023-12-29 02:22:23 +0530

    feat: order details & search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,20 @@
 import Collection from '@/components/shared/Collection'
+import Search from '@/components/shared/Search';
 import { Button } from '@/components/ui/button'
-import { getAllEvents } from '@/lib/actions/event.actions'
+import { getAllEvents } from '@/lib/actions/event.actions';
+import { SearchParamProps } from '@/types';
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
   const events = await getAllEvents({
-    query: '',
-    category: '',
-    page: 1,
+    query: searchText,
+    category,
+    page,
     limit: 6
   })
 
@@ -18,9 +24,9 @@ export default async function Home() {
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-            <p className='p-regular-20 md:p-regular-24'>Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
-            <Button size='lg' asChild className='button w-full sm:w-fit'>
-              <Link href='#events'>
+            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">
                 Explore Now
               </Link>
             </Button>
@@ -31,26 +37,26 @@ export default async function Home() {
             alt="hero"
             width={1000}
             height={1000}
-            className='max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]'
-            />
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
         </div>
-      </section>
+      </section> 
 
-      <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
-        <h2 className='h2-bold'>Trust by <br /> Thousands of Events</h2>
-        <div className='flex w-full flex-col gap-5 md:flex-row'>
-          Search
-          CategoryFilter
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
         </div>
 
         <Collection 
           data={events?.data}
-          emptyTitle='No events found'
-          emptyStateSubtext='Come back later'
-          collectionType='All_Events'
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
           limit={6}
-          page={1}
-          totalPages={2}
+          page={page}
+          totalPages={events?.totalPages}
         />
       </section>
     </>
